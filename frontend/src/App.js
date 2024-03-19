2c8e79bcf2f785a01cdd3dd296af8e5ee08d4d55
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-20 03:21:01 +0600

    ant desing and bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,9 @@
+import { Button } from 'antd';
 function App() {
 	return (
-		<div className='App'>
+		<div className='App p-5'>
 			<h1>Health.com</h1>
+			<Button type='primary'> Primary Button</Button>
 		</div>
 	);
 }
