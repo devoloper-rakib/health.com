1f68959ce7c9757a17ce72c8c3fab8df290e8661
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-21 02:46:47 +0600

    login and register frontend added

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -1,7 +1,34 @@
+import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
+
 const Login = () => {
+	const onFinish = (values) => {
+		console.log('Received values of form: ', values);
+	};
+
 	return (
-		<div>
-			<h1>Login</h1>
+		<div className='authentication'>
+			<div className='authentication-form card p-3'>
+				<h1 className='card-title'>Welcome Back</h1>
+
+				<Form layout='vertical' onFinish={onFinish}>
+					<Form.Item label='Email' name='email'>
+						<Input placeholder='Enter your Email Address' type='email' />
+					</Form.Item>
+
+					<Form.Item label='Password' name='password'>
+						<Input placeholder='Enter your Password' type='password' />
+					</Form.Item>
+
+					<Button className='primary-button my-2' htmlType='submit'>
+						Login
+					</Button>
+
+					<Link className='anchor ' to='/register'>
+						Note A user, Click here to register
+					</Link>
+				</Form>
+			</div>
 		</div>
 	);
 };
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -1,25 +1,37 @@
 import { Button, Form, Input } from 'antd';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+	const onFinish = (values) => {
+		console.log('Received values of form: ', values);
+	};
+
 	return (
 		<div className='authentication'>
 			<div className='authentication-form card p-3'>
 				<h1 className='card-title'>Nice to Meet You</h1>
 
-				<Form layout='vertical'>
+				<Form layout='vertical' onFinish={onFinish}>
 					<Form.Item label='Name' name='name'>
-						<Input placeholder='Enter your name' />
+						<Input placeholder='Enter your name' type='text' />
 					</Form.Item>
 
 					<Form.Item label='Email' name='email'>
-						<Input placeholder='Enter your Email Address' />
+						<Input placeholder='Enter your Email Address' type='email' />
 					</Form.Item>
 
 					<Form.Item label='Password' name='password'>
-						<Input placeholder='Enter your Password' />
+						<Input placeholder='Enter your Password' type='password' />
 					</Form.Item>
 
-					<Button className='primary-button mt-2'> Register </Button>
+					<Button className='primary-button my-2' htmlType='submit'>
+						{' '}
+						Register{' '}
+					</Button>
+
+					<Link className='anchor ' to='/login'>
+						Click here to Login
+					</Link>
 				</Form>
 			</div>
 		</div>
