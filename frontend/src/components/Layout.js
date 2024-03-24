8ca9ed6c93cf8d6a9963a41aa342489b3ffe76ad
Devoloper_Rakib <rakibhasansohag133@gmail.com>
2024-03-24 13:44:05 +0600

    layout design

---
+++ frontend/src/components/Layout.js
@@ -0,0 +1,16 @@
+import '../layout.css';
+const Layout = ({ children }) => {
+	return (
+		<div className='main'>
+			<div className='d-flex layout'>
+				<div className='sidebar'>sidebar</div>
+				<div className='content'>
+					<div className='header'>header</div>
+					<div className='body'> {children} </div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Layout;
--- frontend/src/components/PublicRoute.js
+++ frontend/src/components/PublicRoute.js
@@ -1,5 +1,7 @@
 import { Navigate } from 'react-router-dom';
 
+// TODO: will verify token and localstorage logic to modify the code
+
 const PublicRoute = (props) => {
 	if (localStorage.getItem('token')) {
 		return <Navigate to='/' />;
